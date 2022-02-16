18a44b84f8bcd2278faab8d5445b61a3c0a76650
Ibrahim Zahir <74900384+ibrahimzahir@users.noreply.github.com>
2022-02-16 12:07:12 +0500

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__logo"></div>
+    </div>
+  );
+}
+
+export default Header;
